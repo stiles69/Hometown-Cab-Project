50d9b85c8ee25c096cd80591cd18da2d92815781
stiles69 <brett.salemink@gmail.com>
2018-09-13 19:03:34 -0500

    Update

--- layouts/default.vue
+++ layouts/default.vue
@@ -93,7 +93,7 @@ export default {
 			roundTripPrice: Constants.roundtripprice,
 			additionalStopPrice: Constants.additionalstopprice,
 			Owner: Constants.ownername,
-			Versioner: Versioner.version
+			Version: Versioner.version
 		}
 	}
 }
