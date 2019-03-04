8af6bd18ddc6d94b50bb0078296c86817a75c27e
stiles69 <brett.salemink@gmail.com>
2019-03-04 06:02:23 -0600

    Update

--- plugins/vue-google-maps.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import * as VueGoogleMaps from '~/node_modules/vue2-google-maps/src/main'
-Vue.use(VueGoogleMaps, {
-	load: {
-		key: 'AIzaSyAqX9uvHyVp3Q_dwW_BOGwWzZ6j_9iIm3w'
-		libraries: 'places'
-	}
-})
